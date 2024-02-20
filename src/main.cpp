96bce471231464477a13e0e09ef1577a8157ba3e
Coffee4Dogs <59121551+Coffee4Dogs@users.noreply.github.com>
2024-02-20 13:15:57 -0600

    Template de codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,147 +1,58 @@
-// Version: 0.0.1
-
-//Librerias:
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
 
-// Using & Typedef
 using std::string;
-using std::fstream;
-using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
 
-//Clase Base
+
 class PERSONA{
-    //Atributos
-    public:
+    protected:
         string Identidad;
+        int Telefono;
         string Nombre;
         string ApellidoA;
         string ApellidoB;
-        int Telefono;
-        string NombreCompleto;
 
     public:
-    //Constructor
-    PERSONA(string Identidad, string Nombre, string ApellidoA,string ApellidoB,int Telefono){
-        this-> Identidad = Identidad;
-        this-> Nombre = Nombre;
-        this-> ApellidoA = ApellidoA;
-        this-> ApellidoB = ApellidoB;
-        this-> Telefono = Telefono;
+    // Constructor
+    PERSONA(string Identidad, int Telefono, string Nombre, string ApellidoA, string ApellidoB){
+        this->Identidad = Identidad;
+        this->Telefono = Telefono;
+        this->Nombre = Nombre;
+        this->ApellidoA = ApellidoA;
+        this->ApellidoB = ApellidoB;
     }
-    
-    //Metodo Vacio
+
+    //Metodos Set
+    void setIdentidad(string Identidad){
+        this->Identidad = Identidad;
+    }
+
+
+    // Metodos Get
+    string getIdentidad(){
+        return this->Identidad;
+    }
+
+
+
+    // Metodo vacio
     PERSONA(){}
     
-    //Destructor
+    // Destructor
     ~PERSONA(){}
 
-
-};
-
-// Clase Derivada
-class DOCTOR : public PERSONA {
-    //Atributos:
-    public:
-        string User;
-        string Password;
-        // Hora de Entrada
-        int HoraEntrada;
-        int MinutosEntrada;
-        // Hora de Salida
-        int HoraSalida;
-        int MinutosSalida;
-
-    public:
-    //Constructor:
-    DOCTOR(string User,string Password,int HoraEntrada,int MinutosEntrada,int HoraSalida,int MinutosSalida){
-        this-> User = User;
-        this-> Password = Password;
-        this-> HoraEntrada = HoraEntrada;
-        this-> MinutosEntrada = MinutosEntrada;
-        this-> HoraSalida = HoraSalida;
-        this-> MinutosSalida = MinutosSalida;    
-    }
-
-    void setUserData(string Identidad, string User, string Password){
-        this->Identidad = Identidad;
-        this->User = User;
-        this->Password = Password;
-    }
-
-    //Metodo Vacio
-    DOCTOR(){}
-
-    //Destructor
-    ~DOCTOR(){} 
-};
-
+}; //Fin de la clase PERSONA
 
 
 int main() {
+    PERSONA BobEsponja;
+    //setIdentidad:
+    BobEsponja.setIdentidad("0801-1997-1234");
 
-    DOCTOR doctorcito;
-    fstream File; //Archivo
-    string Line; //Linea del documento.
-    std::vector<string> ListaDoctores;
+    // getIdentidad:
+    std::cout << BobEsponja.getIdentidad() << '\n';
     
-
-    //Asignar un usuario con setUserData()
-    doctorcito.setUserData("080119981234" ,"doctorcito", "1234");
-    
-
-    // // Crear archivo con usuario y contraseña
-    //     File.open("user.dat", fstream::out);
-    //     if(File.is_open()){
-    //         File << "User" << '\n';
-    //         File << doctorcito.User << '\n';
-    //         File << "Password" << '\n';
-    //         File << doctorcito.Password << '\n';
-    //         File.close();
-    //     }
-
-    string UserInput, PasswordInput;
-    std::cout << "Ingresar Usuario: " << '\n';
-    
-    //Leer archivo user.txt
-    File.open("user.dat", fstream::in);
-    if(File.is_open()){
-        std::cout << "~ Leer base de datos" << '\n';
-        while(getline(File, Line)){
-            
-
-            //Si encontramos un Usuario
-            if("User" == Line){
-                std::cout << "User found" << '\n';
-                getline(File, Line); //Pasar a la siguiente linea
-                ListaDoctores.push_back(Line);
-
-                getline(File, Line); //Pasar a la siguiente linea
-                ListaDoctores.push_back(Line);
-
-                getline(File, Line); //Pasar a la siguiente linea
-                ListaDoctores.push_back(Line);
-                
-
-            
-            }
-            
-        }
-
-        std::cout << ListaDoctores[0] << '\n';
-        std::cout << ListaDoctores[1] << '\n';
-        std::cout << ListaDoctores[2] << '\n';
-
-        std::cout << doctorcito.Identidad << '\n';
-
-        File.close(); //Cerrar el archivo user.txt
-    }
-    
-    
-
-
-    std::cout << "Fin del programa" << '\n';
     return 0;
 }
